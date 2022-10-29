23ff367684c9a66e3e649456f51e628ed5ddc774
OtavioFicagna <oaugusto796@gmail.com>
2022-10-29 11:54:03 -0300

    Added funtions: Pushi, Push, Pop

--- AOC-Work.cpp
+++ AOC-Work.cpp
@@ -10,7 +10,7 @@ struct memory
 };
 
 memory mem;
-int error_count = 0; 
+int error_count = 0;
 std::stack<float> stack;
 
 std::string lowercase(std::string &str)
@@ -28,7 +28,7 @@ int identify_operation(std::string operacao)
 {
     const int qtdOps = 14;
     int count = 0;
-    std::string operations[qtdOps] = {"pushi", "push", "pop", "input", "print", "add", "sub", "mul", "div","swap", "drop", "dup", "hlt", "erro"};
+    std::string operations[qtdOps] = {"pushi", "push", "pop", "input", "print", "add", "sub", "mul", "div", "swap", "drop", "dup", "hlt", "erro"};
     while (count < qtdOps)
     {
         if (operations[count] == operacao)
@@ -40,64 +40,122 @@ int identify_operation(std::string operacao)
     return 13;
 }
 
+void pushi()
+{
+    if (stack.size() <= 16)
+    {
+        float value;
+        std::cin >> value;
+        stack.push(value);
+    }
+    else
+    {
+        std::cout << "Stack Overflow\n";
+        error_count++;
+    }
+}
+
+void push()
+{
+    int adress;
+    float value;
+    std::cin >> std::hex >> adress;
+    if (adress > 256)
+    {
+        std::cout << "Index Out Of Range\n";
+        error_count++;
+    }
+    else
+    {
+        value = mem.values[adress - 1];
+        stack.push(value);
+    }
+}
+
+void pop()
+{
+    int adress;
+    std::cin >> std::hex >> adress;
+    if (adress > 256)
+    {
+        std::cout << "Index Out Of Range\n";
+        error_count++;
+    }
+    else
+    {
+        if(stack.empty())
+        {
+            std::cout << "Stack Underflow\n";
+            error_count++;
+        }
+        else
+        {
+            mem.values[adress - 1] = stack.top();
+            mem.adress.push_back(adress - 1);
+            stack.pop();
+        }
+        
+    }
+}
+
 std::string exec_operation(std::string &operation)
 {
     lowercase(operation);
     switch (identify_operation(operation))
-        {
-        case 0:
-            //pushi(pilha);
-            break;
-        case 1:
-            //push(pilha, memoria, operacao);
-            break;
-        case 2:
-            //pop(pilha, memoria, operacao, memoriaModif);
-            break;
-        case 3:
-            //input(pilha);
-            break;
-        case 4:
-            //print(pilha, operacao);
-            break;
-        case 5:
-            //add(pilha);
-            break;
-        case 6:
-            //sub(pilha);
-            break;
-        case 7:
-            //mul(pilha);
-            break;
-        case 8:
-            //div(pilha, operacao);
-            break;
-        case 9:
-            //swap(pilha);
-            break;
-        case 10:
-            //drop(pilha, operacao);
-            break;
-        case 11:
-            //dup(pilha);
-            break;
-        case 12:
-            operation = "hlt";
-            break;
-        case 13:
-            //operacao = "erro";
-            break;
-        }
+    {
+    case 0:
+        pushi();
+        break;
+    case 1:
+        push();
+        break;
+    case 2:
+        pop();
+        break;
+    case 3:
+        // input(pilha);
+        break;
+    case 4:
+        // print(pilha, operacao);
+        break;
+    case 5:
+        // add(pilha);
+        break;
+    case 6:
+        // sub(pilha);
+        break;
+    case 7:
+        // mul(pilha);
+        break;
+    case 8:
+        // div(pilha, operacao);
+        break;
+    case 9:
+        // swap(pilha);
+        break;
+    case 10:
+        // drop(pilha, operacao);
+        break;
+    case 11:
+        // dup(pilha);
+        break;
+    case 12:
+        operation = "hlt";
+        break;
+    case 13:
+        operation = "error";
+        error_count++;
+        break;
+    }
     return operation;
 }
 
 int main()
 {
-    std::string operation; 
+    std::string operation;
     do
     {
         std::cin >> operation;
         exec_operation(operation);
     } while (operation != "hlt" && error_count < 1);
-    
 }
